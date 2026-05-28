08aab24e93a55fc3fedd07782568ea376ea2983f
01felipedias <philipe0905@gmail.com>
2026-05-28 09:15:42 -0300

    primeira alteração do corpo da pagina principal

---
+++ script.js
@@ -0,0 +1,8 @@
+// Comportamento da página
+
+function mostrarMensagem() {
+  alert("Botão clicado! Funcionou! 🎉");
+}
+
+// Isso roda assim que a página abre
+console.log("Página carregada com sucesso!");
